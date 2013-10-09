f757d0ac36d4edae4d296f96588764f5beadecd3
Brandon DeRosier <btd@cheesekeg.com>
2013-10-09 15:25:25 -0400

    Added simplex noise terrain test

---
+++ simplexmap/simplexmap.js
@@ -0,0 +1,62 @@
+var renderer, camera, scene;
+var simplex;
+
+function start() {
+    renderer = new THREE.WebGLRenderer();
+    camera = new THREE.PerspectiveCamera(
+	45, window.innerWidth/window.innerHeight, 0.1, 1000
+    );
+    scene = new THREE.Scene();
+
+    scene.add(camera);
+    camera.position.y = 10;
+    camera.position.z - 100;
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    simplex = new SimplexNoise();
+    addSimplexPlane(0, 0, 100, 100, 0.1, 5);
+
+    {
+	var light = new THREE.PointLight(0xffffff);
+	light.position.y = 15;
+
+	scene.add(light);
+    }
+
+    renderer.render(scene, camera);
+}
+
+function addSimplexPlane(x, z, size, lengthSegments, simplexRatio, simplexAmplitude) {
+    // Generate plane mesh
+    var mat = new THREE.MeshPhongMaterial({
+	color: 0xffffff
+    });
+    var mesh = new THREE.Mesh(
+	new THREE.PlaneGeometry(size, size, lengthSegments, lengthSegments), mat
+    );
+
+    mesh.rotation.x = -Math.PI/2
+    mesh.position.x = x;
+    mesh.position.z = z;
+
+    // Offset geometry with simplex noise
+    // Plane geometry total vertices length = (size + 1)^2
+    var lVerts = lengthSegments + 1;
+    var segSize = size/lengthSegments;
+    for (var gz = 0; gz < lVerts; gz++)
+	for (var gx = 0; gx < lVerts; gx++)
+	    mesh.geometry.vertices[gz*lVerts + gx].y += simplex.noise2D(
+		(x + gx*segSize)*simplexRatio, (z + gz*segSize)*simplexRatio
+	    )*simplexAmplitude;
+
+    mesh.geometry.computeFaceNormals();
+    mesh.geometry.computeVertexNormals();
+
+    scene.add(mesh);
+}
+
+function update() {
+    requestAnimFrame(update);
+}
