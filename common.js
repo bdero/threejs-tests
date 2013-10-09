f26056a7374de3267a2c3328bd2e9357d1b555e9
Brandon DeRosier <btd@cheesekeg.com>
2013-10-09 15:24:30 -0400

    Added some wrapper functions for pointer locking

--- common.js
+++ common.js
@@ -1,3 +1,5 @@
+// Device-friendly animation
+
 window.requestAnimFrame = (function(){
     return  window.requestAnimationFrame ||
 	window.webkitRequestAnimationFrame ||
@@ -6,3 +8,19 @@ window.requestAnimFrame = (function(){
             window.setTimeout(callback, 1000 / 60);
         };
 })();
+
+// Pointer lock API
+
+window.havePointerLock =
+    'pointerLockElement' in document ||
+    'mozPointerLockElement' in document ||
+    'webkitPointerLockElement' in document;
+
+if (window.havePointerLock) {
+    var windowElement = window.Element.prototype;
+    windowElement.pointerLockElement = 
+	windowElement.requestPointerLock ||
+	windowElement.mozRequestPointerLock ||
+	windowElement.webkitRequestPointerLock;
+} else
+    console.log("Pointer lock not supported by browser.");
